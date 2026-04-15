15cc03cc06afce4221015344891edbd1400f8679
wiyr0 <wangyixian3@huawei.com>
2026-04-15 16:00:30 +0800

    [bugfix][sync] 修复低版本cmake编译msdebug报错问题
    Co-authored-by: wangyixian<wangyixian3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !58 merge fix_build_26_0_0 into 26.0.0
    
    [bugfix][sync] 修复低版本cmake编译msdebug报错问题
    
    Created-by: wiyr0
    Commit-by: wangyixian
    Merged-by: ascend-robot
    Description: ### 1. 修改描述
    - **修改原因：**  
    - **修改方案：**
    - **修改内容：**
    - [ ] **涉及代码双合**(贴上另一个PR链接)：https://gitcode.com/Ascend/msot/pull/57
    ----
    
    ### 2. 功能验证
    - [ ] **功能自验截图**（请确保不体现个人信息）  
    - [ ] **冒烟是否通过**  
    ----
    
    ### 3. 代码检视
    - **要求：**
      - 合入功能代码大于 200 行，需要sig会议申报代码检视议题，并在PR中标注会议。
      - committer评估是否需要在sig会议进行代码检视。
      - 参与检视的committer人员名单与检视时间。
      - 大于 1000 行代码原则上不允许合入，需进行备案。
    - [ ] **是否经过代码检视**  
    - [ ] **是否具备UT测试用例看护**
    - [ ] **是否需要在sig会议中进行代码检视**
    - **检视committer人员名单与检视时间：**
    ----
    ### 4. 资料修改自检
    - **资料修改：**    
    ----
    
    See merge request: Ascend/msot!58

--- cmake/module/msdebug.cmake
+++ cmake/module/msdebug.cmake
@@ -1,21 +1,20 @@
 set(MSDEBUG_PATH ${ROOT_DIR}/msdebug)
-set(ARGS
-    "-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/msdebug"
-    "-DCMAKE_BUILD_TYPE=Release"
-)
-
+set(ARGS "-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/msdebug"
+    "-DCMAKE_BUILD_TYPE=Release")
+set(GENERATOR "-G" "Unix Makefiles")
 # 查找 Ninja
 find_program(NINJA ninja)
 if(NINJA)
-    list(APPEND ARGS "-G Ninja")  # 使用 list(APPEND) 追加，避免覆盖
+    set(GENERATOR "-G" "Ninja")
     set(BUILD_CMD ${NINJA})
 else()
     set(BUILD_CMD $(MAKE))
 endif()
+
 add_custom_command(
         OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/msdebug_build.done
         COMMAND ${CMAKE_COMMAND} -E make_directory build_msdebug
-        COMMAND cd build_msdebug && cmake ${MSDEBUG_PATH} ${ARGS}
+        COMMAND cd build_msdebug && cmake ${GENERATOR} ${ARGS} ${MSDEBUG_PATH}
         COMMAND cd build_msdebug && unset MAKEFLAGS && ${BUILD_CMD}
         COMMAND touch ${CMAKE_CURRENT_BINARY_DIR}/msdebug_build.done
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
